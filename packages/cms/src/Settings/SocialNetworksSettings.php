7751dd8facf5945e2186c90aa676a498c207edb0
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-03-05 23:13:13 +0000

    wip

--- packages/cms/src/Settings/SocialNetworksSettings.php
+++ packages/cms/src/Settings/SocialNetworksSettings.php
@@ -6,13 +6,13 @@ use Spatie\LaravelSettings\Settings;
 
 class SocialNetworksSettings extends Settings
 {
-    public string $facebook;
-    public string $twitter;
-    public string $instagram;
-    public string $pinterest;
-    public string $tiktok;
-    public string $linkedin;
-    public string $youtube;
+    public ?string $facebook;
+    public ?string $twitter;
+    public ?string $instagram;
+    public ?string $pinterest;
+    public ?string $tiktok;
+    public ?string $linkedin;
+    public ?string $youtube;
 
     public static function group(): string
     {
