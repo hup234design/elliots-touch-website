008ec8a32146999196258570764c547cfdd5461a
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-04-23 17:15:55 +0100

    cleaning up post page

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -9,12 +9,12 @@
 
           <h1>{{ $post->title }}</h1>
 
-          <p class="flex items-center gap-x-4 text-sm">
-              <time datetime="2020-03-16" class="text-gray-500">
+          <p class="flex items-center gap-x-4">
+              <time datetime="2020-03-16" class="text-gray-700">
                   {{ $post->published_at->format('j F Y') }}
               </time>
               @if($category = $post->postCategory)
-                  <a href="javascript: void;" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                  <a href="javascript: void;" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-800 hover:bg-gray-100">
                       {{ $category->title }}
                   </a>
               @endif
